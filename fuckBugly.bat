5d7bf985bc2289326b12528f5a3b244c8d55df6c
Do <dujianchi@lppstore.com>
2022-04-21 17:22:48 +0800

    运行环境

--- fuckBugly.bat
+++ fuckBugly.bat
@@ -2,4 +2,6 @@
 set arg1=%1
 set arg2=%2
 set pwd=%~dp0
-"%pwd%\jre\bin\java.exe" -jar "%pwd%\buglyqq-upload-symbol.jar" -appid 0901e6ad80 -appkey d5a05e05-4f46-4c69-9d7d-ac5e8b7d9afe -version "%arg1%" -bundleid com.gsd.yd.xxkm -platform Android -inputMapping "%arg2%"
+set appid=你的appId
+set appkey=你的appkey
+"%pwd%\jre\bin\java.exe" -jar "%pwd%\buglyqq-upload-symbol.jar" -appid %appid% -appkey %appkey% -version "%arg1%" -bundleid com.gsd.yd.xxkm -platform Android -inputMapping "%arg2%"
---
+++ readme.txt
@@ -0,0 +1,3 @@
+放入环境变量，然后命令行 运行 
+
+fuckBugly.bat "版本号" "mapping所在文件夹"
