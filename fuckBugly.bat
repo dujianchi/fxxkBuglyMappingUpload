137547d175dfb2a3c5922ce79a517b4b3551855b
dujianchi <dujianchi512@gmail.com>
2022-06-11 04:48:03 +0800

    bundleid

--- fuckBugly.bat
+++ fuckBugly.bat
@@ -3,7 +3,8 @@ set version=%1
 set mappingdir=%2
 set appid=%3
 set appkey=%4
+set bundleid=%5
 
 set pwd=%~dp0
 
-"%pwd%\jre\bin\java.exe" -jar "%pwd%\buglyqq-upload-symbol.jar" -appid %appid% -appkey %appkey% -version "%version%" -bundleid com.gsd.yd.xxkm -platform Android -inputMapping "%mappingdir%"
+"%pwd%\jre\bin\java.exe" -jar "%pwd%\buglyqq-upload-symbol.jar" -appid %appid% -appkey %appkey% -version "%version%" -bundleid "%bundleid%" -platform Android -inputMapping "%mappingdir%"
